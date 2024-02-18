4f520d10033ec4c3738a6d0aac04e671b15af535
arpan-kumar-saini <arpansaini55@gmail.com>
2024-02-18 16:39:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import Herosection from './components/Herosection'
+
+function App() {
+  return (
+    <>
+    <div className='bg-[#202124] h-screen'>
+    <Header/>
+    <Herosection/>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Herosection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Herosection() {
+  return (
+    <div className='h-screen bg-[#202124]'>Herosection</div>
+  )
+}
+
+export default Herosection
